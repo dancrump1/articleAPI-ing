1916ef1ef6e975bb2239845bc87aa2c3f08a7de2
Orlando Correa <orlandocorrea12@yahoo.com>
2018-10-20 12:55:58 -0400

    added some changes to js

---
+++ javascript/app.js
@@ -0,0 +1,21 @@
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += "?" + $.param({
+    'api-key': "04b445e59edf48f780cbc46b2bfe006c"
+});
+
+
+$.ajax({
+    url: url,
+    method: 'GET',
+}).then(function (response) {
+    var result = response.response.docs;
+    $("#submit").click(function () {
+
+
+        for (var i = 0; i < result.length; i++) {
+            console.log(result[i])
+        }
+    }).fail(function (err) {
+        throw err;
+    });
+});
